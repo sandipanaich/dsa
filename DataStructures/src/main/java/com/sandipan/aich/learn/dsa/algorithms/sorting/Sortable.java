afbae2ddc40a2566d8fa2094f9934bb6fd6e26cc
Sandipan Aich <sandipanaich@gmail.com>
2025-12-06 22:54:02 -0500

    Implementation of the Heap Sort Algorithm.

--- DataStructures/src/main/java/com/sandipan/aich/learn/dsa/algorithms/sorting/Sortable.java
+++ DataStructures/src/main/java/com/sandipan/aich/learn/dsa/algorithms/sorting/Sortable.java
@@ -1,5 +1,6 @@
 package com.sandipan.aich.learn.dsa.algorithms.sorting;
 
+@FunctionalInterface
 public interface Sortable<T extends Comparable<T>> {
 
     void sort(T[] array);
@@ -8,5 +9,9 @@ public interface Sortable<T extends Comparable<T>> {
         T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    default boolean less(T a, T b) {
+        return a.compareTo(b) < 0;
     }
 }
